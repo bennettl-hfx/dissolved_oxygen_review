1c9c2f9de2bf3f0a86c87df08f1e5ad38706df61
Danielle Dempsey <ddempsey@perennia.ca>
2024-05-22 10:21:03 -0300

    initial commit

---
+++ R/2_do_percent_sat_analysis.R
@@ -0,0 +1,15 @@
+# May 22, 2024
+
+library(dplyr)
+library(here)
+library(ggplot2)
+
+
+# read in do data ---------------------------------------------------------
+
+dat <- readRDS(here("data/dissolved_oxygen_percent_saturation.rds"))
+
+
+# explore -----------------------------------------------------------------
+
+
